bddd59602c901cf1cb80f5b3d903d77ec6addd40
Ilias Rafael <iliasrafael@gmail.com>
2017-12-27 17:21:03 +0200

    Some changes

--- host/slimit/eventPage.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(function(){
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "https://refraction.tech/calculator/newUser.php?info="+chrome.app.getDetails().id, true);
-	xhttp.send();
-})
